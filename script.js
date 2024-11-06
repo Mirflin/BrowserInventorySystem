0187347b1fcdd34951b7c7ede05199501baa947d
Miirflin <mrmiirflinn@gmail.com>
2024-11-07 00:35:08 +0200

    now can create objects and add it to the inventory

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:077c8ac21f95b6955b95e69868633739f4bfc5079677881453213cbe52abb4bb
-size 1451
+oid sha256:adc1fb6f2b524bf369fa90c79d6b0a37a14a9e16aef2a63d388533a48656265e
+size 12503
