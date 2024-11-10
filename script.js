642652f80e58b7f6ef54e8ca8a104b9e7c1dfb64
Miirflin <mrmiirflinn@gmail.com>
2024-11-10 17:53:29 +0200

    save and load function for JSON files

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:761f2b2be87b9f94d6c19834e22b3a26357161472802f34864b700412415782a
-size 24035
+oid sha256:5cce0e14ed4e6d44abaffeb0b71cf63b391a6bcb1045a24ed2c9969dda3dda84
+size 29224
