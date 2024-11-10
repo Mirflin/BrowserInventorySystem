cbd6e066baa611fe8be09af4a66eff1278a457eb
Miirflin <mrmiirflinn@gmail.com>
2024-11-10 18:32:52 +0200

    bronze, silver, gold coin save feature

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cce0e14ed4e6d44abaffeb0b71cf63b391a6bcb1045a24ed2c9969dda3dda84
-size 29224
+oid sha256:b7a55b91e022cb1651ae43d722e8f599ff2ed10c3c7e68f5f4322814247a5f8b
+size 29741
